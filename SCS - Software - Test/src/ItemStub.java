4e5a999c06a8090240159703bc7ee5781ba01e36
MuHe200206 <99844456+MuHe200206@users.noreply.github.com>
2022-03-19 21:10:54 -0600

    Creating a ItemStub

---
+++ SCS - Software - Test/src/ItemStub.java
@@ -0,0 +1,13 @@
+import org.lsmr.selfcheckout.Item;
+
+
+public class ItemStub extends Item {
+	
+	public ItemStub(double weightInGrams) {
+		super(weightInGrams);
+	}
+	
+	public double getWeight() {
+		return super.getWeight();
+	}
+}
