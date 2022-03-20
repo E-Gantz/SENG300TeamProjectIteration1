b27b5b839177056b8f88922f33149f7b2fb1ac33
Eric G <65147562+E-Gantz@users.noreply.github.com>
2022-03-20 10:41:18 -0600

    integrated Sean's code

---
+++ SCS - Software - Test/src/scsSoftwareTests/SCSsoftwareTestSuite.java
@@ -0,0 +1,9 @@
+package scsSoftwareTests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({CheckoutTests.class, ItemAdderTests.class, PlacingItemTest.class})
+public class SCSsoftwareTestSuite {}
+
---
+++ SCS - Software/src/scsSoftware/Checkout.java
@@ -0,0 +1,40 @@
+package scsSoftware;
+
+import org.lsmr.selfcheckout.devices.BarcodeScanner;
+import org.lsmr.selfcheckout.devices.SimulationException;
+
+public class Checkout {
+	boolean state;
+	ProductCart pcart;
+	BarcodeScanner scanner;
+	
+	public Checkout(BarcodeScanner scanner, ProductCart pcart) {
+		this.scanner = scanner;
+		this.pcart = pcart;
+		state = false;
+	}
+	
+	/**
+	 * Changes the state to enable checkout to occur
+	 * @throws SimulationException
+	 *             If the scanner is disabled and the bagging area is waiting for items.
+	 * @throws SimulationException
+	 *             If the cart is empty.
+	 */
+	public void enable() {
+		if(scanner.isDisabled()) {
+			throw new SimulationException("Need to place all items in bagging area before proceeding to checkout.");
+		}
+		
+		if(pcart.cart.isEmpty()) {
+			throw new SimulationException("Cart must contain items in order to proceed to checkout.");
+		}
+		
+		scanner.disable();
+		state = true;
+	}
+	public boolean getState() {
+		return this.state;
+	}
+	
+}
