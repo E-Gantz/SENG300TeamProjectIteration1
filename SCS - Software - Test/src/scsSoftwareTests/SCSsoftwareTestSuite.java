0d5b53c71132b7c50312e05e00268f8f2697198a
Eric G <65147562+E-Gantz@users.noreply.github.com>
2022-03-20 20:48:16 -0600

    added louis' test file to suite

--- SCS - Software - Test/src/scsSoftwareTests/SCSsoftwareTestSuite.java
+++ SCS - Software - Test/src/scsSoftwareTests/SCSsoftwareTestSuite.java
@@ -4,6 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({CheckoutTests.class, ItemAdderTests.class, PlacingItemTest.class, TestPayWithBankNote.class, ReceiptTest.class})
+@Suite.SuiteClasses({CheckoutTests.class, ItemAdderTests.class, PlacingItemTest.class, TestPayWithBankNote.class, ReceiptTest.class, CoinPaymentTest.class})
 public class SCSsoftwareTestSuite {}
 
